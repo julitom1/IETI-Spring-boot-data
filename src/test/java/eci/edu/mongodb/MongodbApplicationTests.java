eba424a9c089fc2718c02a0034b9ca234fc4335c
julitom1 <yarit.villalobos@mail.escuelaing.edu.co>
2021-08-31 10:26:01 -0500

    iniciar

---
+++ src/test/java/eci/edu/mongodb/MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
